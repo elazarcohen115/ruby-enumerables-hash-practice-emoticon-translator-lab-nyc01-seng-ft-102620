8a5cefa31d2a9b109132fd116faf7833998e2e92
simon cohen <escohen115@gmail.com>
2020-10-19 13:42:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,9 @@ emoticons.each do |english_word, emoticon_set|
 emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
 emoticon_hash["get_meaning"][emoticon_set.last] = english_word
 end
+emoticon_hash
+end
+
 
 def get_japanese_emoticon
   # code goes here
